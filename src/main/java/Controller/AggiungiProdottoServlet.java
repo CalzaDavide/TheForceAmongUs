debb792d0208bbf782ba2e12dc3883eed7e3ea37
CalzaDavide <davide.calza.2002@gmail.com>
2023-05-03 09:35:29 +0200

    Sblonf

--- src/main/java/Controller/AggiungiProdottoServlet.java
+++ src/main/java/Controller/AggiungiProdottoServlet.java
@@ -11,7 +11,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet(name = "AggiungiProdottoServlet" value = "/aggiungi-prodotto")
+@WebServlet(name = "AggiungiProdottoServlet", value = "/aggiungi-prodotto")
 public class AggiungiProdottoServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
